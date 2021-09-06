030a721aa45db056b00ff8e21d93db30b3917fd9
jwoo820 <pjwoo820@gmail.com>
2021-09-06 13:00:28 +0900

    0906 Get User Position

--- Assets/Script/GetCameraPos.cs
+++ Assets/Script/GetCameraPos.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class GetCameraPos : MonoBehaviour
 {
-    public GameObject _arCameraObjcet;
+    public Camera _camera;
 
+    public static Vector3 _userPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _camera = GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(_arCameraObjcet.position)
+        _userPos = _camera.transform.position;
+        Debug.Log("x : " + _userPos.x + ", y : " + _userPos.y + ", z : " + _userPos.z);
     }
 }
